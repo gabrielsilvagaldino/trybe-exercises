36448c63ca2961478d9770bac2618a793839abf8
gabrielsilvagaldino <gabrielgaldino3004@gmail.com>
2022-08-17 16:02:26 -0300

    Exercicio 2 - Mostrar informações de um array de objetos na tela, usando props

--- front-end/bloco-10-introducao-a-react/dia-2-componentes-react/imagem-gatinho/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-2-componentes-react/imagem-gatinho/src/App.js
@@ -1,17 +1,37 @@
-import './App.css';
 import React from 'react';
+import './App.css';
+import Order from './Order';
 
-class Image extends React.Component {
+class App extends React.Component {
   render() {
-    return <img width={900} src={this.props.source} alt={this.props.alternativeText} />;
+    const produtos = [{
+      id: 102,
+      user: "cena@gmail.com",
+      product: "Razer Headphone",
+      price: {
+        value: 99.99,
+        currency: "dollars"
+      }
+    },
+    {
+      id: 77,
+      user: "cena@gmail.com",
+      product: "Monster 500mL",
+      price: {
+        value: 9.99,
+        currency: "dollars"
+      }
+    }];
+
+    return (
+      produtos.map((item) => (
+      <div className="App">
+        <h1> Orders recently created </h1>
+         <Order order={item}/>
+      </div>
+      ))
+    );
   }
-}
-
-
-function App() {
-  return (
-    <Image source='https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg' alternativeText='Cute cat staring'/>
-  );
 }
 
 export default App;
